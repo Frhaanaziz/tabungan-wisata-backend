e51debba14e89efc5efd0f6b9a3fae9e41db36ee
Farhan Aziz <farhan@aththariq.com>
2024-01-24 21:13:58 +0700

    Add CreateTransactionDto class to handle transaction details and customer details

---
+++ src/midtrans/dto/create-transaction.dto.ts
@@ -0,0 +1,44 @@
+import {
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+
+class TransactionDetails {
+  @IsString()
+  @IsNotEmpty()
+  order_id: string;
+
+  @IsInt()
+  @Min(0)
+  gross_amount: number;
+}
+
+class CustomerDetails {
+  @IsString()
+  first_name: string;
+
+  @IsString()
+  last_name?: string;
+
+  @IsEmail()
+  email: string;
+}
+
+export class CreateTransactionDto {
+  @IsString()
+  baseUrl: string;
+
+  @IsString()
+  paymentId: string;
+
+  transaction_details: TransactionDetails;
+
+  customer_details: CustomerDetails;
+
+  @IsOptional()
+  item_details?: object | object[];
+}
