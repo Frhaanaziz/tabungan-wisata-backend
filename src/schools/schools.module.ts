013824719699486aaadbc46d228744d45c8b4a52
Farhan Aziz <farhan@aththariq.com>
2024-01-25 15:20:12 +0700

    Add EventsModule and EventsService to SchoolsModule

--- src/schools/schools.module.ts
+++ src/schools/schools.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { SchoolsService } from './schools.service';
 import { SchoolsController } from './schools.controller';
 import { UtilsModule } from 'src/utils/utils.module';
+import { EventsModule } from 'src/events/events.module';
+import { EventsService } from 'src/events/events.service';
 
 @Module({
   imports: [UtilsModule],
   controllers: [SchoolsController],
-  providers: [SchoolsService],
+  providers: [SchoolsService, EventsService],
   exports: [SchoolsService],
 })
 export class SchoolsModule {}
