131b40b13eea199e8e40158a8fe802d46a21cc66
Farhan Aziz <farhan@aththariq.com>
2024-01-26 16:52:47 +0700

    Add PrismaClientExceptionFilter to global filters

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const { httpAdapter } = app.get(HttpAdapterHost);
 
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   await app.listen(process.env.PORT);
 }
 bootstrap();
