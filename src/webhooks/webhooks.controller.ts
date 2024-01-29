2b1ebac2916edceae3cbe25773069f81b9e5c69d
Farhan Aziz <farhan@aththariq.com>
2024-01-29 22:27:56 +0700

    Add webhooks controller for handling Midtrans payment notifications

---
+++ src/webhooks/webhooks.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { WebhooksService } from './webhooks.service';
+import { Public } from 'src/auth/public.decorator';
+import { MidtransNotificationDto } from './dto/midtrans-notification.dto';
+import { PaymentsService } from 'src/payments/payments.service';
+
+@Controller('webhooks')
+export class WebhooksController {
+  private readonly log = new Logger(WebhooksController.name);
+  constructor(
+    private readonly webhooksService: WebhooksService,
+    private readonly paymentsService: PaymentsService,
+  ) {}
+
+  @Public()
+  @Post('midtrans-notification')
+  async midtransPaymentNotification(@Body() body: MidtransNotificationDto) {
+    this.log.verbose('Midtrans payment notification received');
+    const { order_id } = body;
+
+    const payment = await this.paymentsService.getPayment({ id: order_id });
+    if (payment) {
+      await this.webhooksService.updateStatusPayment(body, payment);
+    }
+
+    return 'OK';
+  }
+}
