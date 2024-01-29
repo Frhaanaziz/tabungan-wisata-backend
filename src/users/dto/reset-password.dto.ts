1a117b0aab4a7f9f4c8748a4c739d453e8aa9660
Farhan Aziz <farhan@aththariq.com>
2024-01-29 14:46:19 +0700

    Remove userId validation in ResetPasswordDto

--- src/users/dto/reset-password.dto.ts
+++ src/users/dto/reset-password.dto.ts
@@ -1,11 +1,7 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
 export class ResetPasswordDto {
   @IsString()
   @MinLength(6)
   newPassword: string;
-
-  @IsString()
-  @IsNotEmpty()
-  userId: string;
 }
