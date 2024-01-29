1931ba25fe72ebbac997186675d1e44f3e2b813e
Farhan Aziz <farhan@aththariq.com>
2024-01-29 22:28:10 +0700

    Add MidtransNotificationDto class

---
+++ src/webhooks/dto/midtrans-notification.dto.ts
@@ -0,0 +1,44 @@
+import { IsOptional } from 'class-validator';
+
+export class MidtransNotificationDto {
+  // Payment notification
+  @IsOptional()
+  transaction_status: string;
+
+  @IsOptional()
+  order_id: string;
+
+  @IsOptional()
+  status_code: string;
+
+  @IsOptional()
+  gross_amount: string;
+
+  @IsOptional()
+  signature_key: string;
+
+  @IsOptional()
+  fraud_status: string;
+
+  @IsOptional()
+  payment_type: string;
+
+  // Payout notification
+  @IsOptional()
+  reference_no: string;
+
+  @IsOptional()
+  amount: string;
+
+  @IsOptional()
+  status: string;
+
+  @IsOptional()
+  updated_at: string;
+
+  @IsOptional()
+  error_code: string;
+
+  @IsOptional()
+  error_message: string;
+}
