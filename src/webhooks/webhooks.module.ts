ec6dddaaebdf618f4dfa34cd0dd9eb95d359f88f
Farhan Aziz <farhan@aththariq.com>
2024-01-30 16:22:32 +0700

    Add UsersModule to WebhooksModule imports

--- src/webhooks/webhooks.module.ts
+++ src/webhooks/webhooks.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { WebhooksService } from './webhooks.service';
 import { WebhooksController } from './webhooks.controller';
 import { PaymentsModule } from 'src/payments/payments.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [PaymentsModule, UsersModule],
   controllers: [WebhooksController],
   providers: [WebhooksService],
 })
