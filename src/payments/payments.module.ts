f56a7b583aed4de134e37548f386356624b8cc98
Farhan Aziz <farhan@aththariq.com>
2024-01-31 16:31:39 +0700

    Add EventsModule to PaymentsModule imports

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -4,9 +4,15 @@ import { PaymentsController } from './payments.controller';
 import { UtilsModule } from 'src/utils/utils.module';
 import { UsersModule } from 'src/users/users.module';
 import { MidtransModule } from 'src/midtrans/midtrans.module';
+import { EventsModule } from 'src/events/events.module';
 
 @Module({
-  imports: [UtilsModule, forwardRef(() => UsersModule), MidtransModule],
+  imports: [
+    UtilsModule,
+    forwardRef(() => UsersModule),
+    MidtransModule,
+    EventsModule,
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService],
