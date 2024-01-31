d3659b44ab83a84e8a6be96ec29557aceae95efe
Farhan Aziz <farhan@aththariq.com>
2024-01-31 16:15:14 +0700

    Add UsersModule and SchoolsModule imports to WithdrawalsModule

--- src/withdrawals/withdrawals.module.ts
+++ src/withdrawals/withdrawals.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { WithdrawalsService } from './withdrawals.service';
 import { WithdrawalsController } from './withdrawals.controller';
 import { UtilsModule } from 'src/utils/utils.module';
+import { UsersModule } from 'src/users/users.module';
+import { SchoolsModule } from 'src/schools/schools.module';
 
 @Module({
-  imports: [UtilsModule],
+  imports: [UtilsModule, UsersModule, SchoolsModule],
   controllers: [WithdrawalsController],
   providers: [WithdrawalsService],
 })
