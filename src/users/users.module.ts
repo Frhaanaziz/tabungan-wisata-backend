333b2df1d73c312852c75f8a4386edc7213e17af
Farhan Aziz <farhan@aththariq.com>
2024-02-01 22:39:52 +0700

    Remove import of SchoolsModule in users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,14 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UtilsModule } from 'src/utils/utils.module';
 import { VerificationsModule } from 'src/verifications/verifications.module';
-import { SchoolsModule } from 'src/schools/schools.module';
 import { PaymentsModule } from 'src/payments/payments.module';
 
 @Module({
   imports: [
     UtilsModule,
     forwardRef(() => VerificationsModule),
-    SchoolsModule,
     forwardRef(() => PaymentsModule),
   ],
   controllers: [UsersController],
