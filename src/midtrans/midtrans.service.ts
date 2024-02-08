aa3d27ddc1a6659d772a7a3cfc8245c23f04db47
Farhan Aziz <farhan@aththariq.com>
2024-02-08 12:51:42 +0700

    Refactor MidtransService to use axios directly for API calls

--- src/midtrans/midtrans.service.ts
+++ src/midtrans/midtrans.service.ts
@@ -1,9 +1,22 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { getMidtransApp } from 'src/utils/axios';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
+import axios from 'axios';
 
 @Injectable()
 export class MidtransService {
+  private getMidtransApp() {
+    const authString = btoa(`${process.env.MIDTRANS_SERVER_KEY}:`);
+
+    return axios.create({
+      baseURL: process.env.MIDTRANS_APP_URL,
+      headers: {
+        Authorization: `Basic ${authString}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+  }
+
   async createTransaction(payload: CreateTransactionDto) {
     const { baseUrl, paymentId, ...restPayload } = payload;
 
@@ -20,7 +33,10 @@ export class MidtransService {
       },
     };
 
-    const res = await getMidtransApp().post(`/snap/v1/transactions`, input);
+    const res = await this.getMidtransApp().post(
+      `/snap/v1/transactions`,
+      input,
+    );
     if (res.status !== 201)
       throw new InternalServerErrorException('Failed to create transaction');
 
