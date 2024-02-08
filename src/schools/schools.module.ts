ac5063f02026719a0c074581b76363b2298ef3df
Farhan Aziz <farhan@aththariq.com>
2024-02-08 16:48:03 +0700

    Remove EventsModule and EventsService from SchoolsModule

--- src/schools/schools.module.ts
+++ src/schools/schools.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { SchoolsService } from './schools.service';
 import { SchoolsController } from './schools.controller';
 import { UtilsModule } from 'src/utils/utils.module';
-import { EventsModule } from 'src/events/events.module';
-import { EventsService } from 'src/events/events.service';
 
 @Module({
   imports: [UtilsModule],
   controllers: [SchoolsController],
-  providers: [SchoolsService, EventsService],
+  providers: [SchoolsService],
   exports: [SchoolsService],
 })
 export class SchoolsModule {}
