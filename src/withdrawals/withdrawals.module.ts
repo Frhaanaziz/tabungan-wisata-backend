fc6cd984756cf2840ffdf383ef06f94d39c7c78e
Farhan Aziz <farhan@aththariq.com>
2024-02-08 16:48:20 +0700

    Remove SchoolsModule import from withdrawals.module.ts

--- src/withdrawals/withdrawals.module.ts
+++ src/withdrawals/withdrawals.module.ts
@@ -3,10 +3,9 @@ import { WithdrawalsService } from './withdrawals.service';
 import { WithdrawalsController } from './withdrawals.controller';
 import { UtilsModule } from 'src/utils/utils.module';
 import { UsersModule } from 'src/users/users.module';
-import { SchoolsModule } from 'src/schools/schools.module';
 
 @Module({
-  imports: [UtilsModule, UsersModule, SchoolsModule],
+  imports: [UtilsModule, UsersModule],
   controllers: [WithdrawalsController],
   providers: [WithdrawalsService],
 })
