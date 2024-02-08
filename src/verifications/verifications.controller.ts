65df03c1880b3fdfc6e4dc8e539838c00abccd39
Farhan Aziz <farhan@aththariq.com>
2024-02-08 12:52:01 +0700

    Add Prisma and getUser method to VerificationsController

--- src/verifications/verifications.controller.ts
+++ src/verifications/verifications.controller.ts
@@ -5,19 +5,31 @@ import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UsersService } from 'src/users/users.service';
 import { UtilsService } from 'src/utils/utils.service';
 import { VerifyTokenDto } from './dto/verify-token.dto';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'nestjs-prisma';
 
 @Controller('verifications')
 export class VerificationsController {
   constructor(
     private readonly verificationsService: VerificationsService,
-    private readonly usersService: UsersService,
     private readonly utilsService: UtilsService,
+    private readonly prisma: PrismaService,
   ) {}
+
+  private async getUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+  }): Promise<User | null> {
+    const { where } = params;
+
+    return this.prisma.user.findUnique({
+      where,
+    });
+  }
 
   @Public()
   @Post('/verify-email')
   async verifyEmail(@Body() { baseUrl, email }: VerifyEmailDto) {
-    const user = await this.usersService.getUser({ where: { email } });
+    const user = await this.getUser({ where: { email } });
     if (!user) throw new BadRequestException('Account does not exist');
 
     return this.verificationsService.verifyEmail({
@@ -32,7 +44,7 @@ export class VerificationsController {
   async verifyEmailForResetPassword(
     @Body() { baseUrl, email }: VerifyEmailDto,
   ) {
-    const user = await this.usersService.getUser({ where: { email } });
+    const user = await this.getUser({ where: { email } });
     if (!user) throw new BadRequestException('Account does not exist');
 
     return this.verificationsService.verifyEmailForResetPassword({
@@ -46,5 +58,12 @@ export class VerificationsController {
   @Post('/verify-token')
   async verifyToken(@Body() { token }: VerifyTokenDto) {
     return this.utilsService.verifyJwtToken(token);
+  }
+
+  @Post('/refresh-token')
+  async refreshToken(@Body() { token }: VerifyTokenDto) {
+    const newToken = this.utilsService.refreshToken(token);
+
+    return { token: newToken };
   }
 }
