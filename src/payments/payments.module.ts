e3f0ba9492e5afba656233773db63488654fecba
Farhan Aziz <farhan@aththariq.com>
2024-02-08 12:51:45 +0700

    Remove unused imports in payments module

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -2,17 +2,10 @@ import { Module, forwardRef } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { UtilsModule } from 'src/utils/utils.module';
-import { UsersModule } from 'src/users/users.module';
 import { MidtransModule } from 'src/midtrans/midtrans.module';
-import { EventsModule } from 'src/events/events.module';
 
 @Module({
-  imports: [
-    UtilsModule,
-    forwardRef(() => UsersModule),
-    MidtransModule,
-    EventsModule,
-  ],
+  imports: [UtilsModule, MidtransModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService],
