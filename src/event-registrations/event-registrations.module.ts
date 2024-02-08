b6da8f8b5ff8db99c7388af2a69411b102b510f7
Farhan Aziz <farhan@aththariq.com>
2024-02-08 16:47:34 +0700

    Add UtilsModule import to EventRegistrationsModule

--- src/event-registrations/event-registrations.module.ts
+++ src/event-registrations/event-registrations.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventRegistrationsService } from './event-registrations.service';
 import { EventRegistrationsController } from './event-registrations.controller';
+import { UtilsModule } from 'src/utils/utils.module';
 
 @Module({
+  imports: [UtilsModule],
   controllers: [EventRegistrationsController],
   providers: [EventRegistrationsService],
 })
