45624fd3811b969154b4ee98006f23bdc835ea4e
Farhan Aziz <farhan@aththariq.com>
2024-02-08 16:48:13 +0700

    Remove UsersModule import from webhooks.module.ts

--- src/webhooks/webhooks.module.ts
+++ src/webhooks/webhooks.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { WebhooksService } from './webhooks.service';
 import { WebhooksController } from './webhooks.controller';
 import { PaymentsModule } from 'src/payments/payments.module';
-import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [PaymentsModule, UsersModule],
+  imports: [PaymentsModule],
   controllers: [WebhooksController],
   providers: [WebhooksService],
 })
