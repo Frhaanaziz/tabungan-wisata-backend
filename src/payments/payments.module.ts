2fc7541eb7e8eedc49893084238f17ca5a52e2a8
Farhan Aziz <farhan@aththariq.com>
2024-02-12 16:40:27 +0700

    Add NotificationsModule to PaymentsModule imports

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { UtilsModule } from 'src/utils/utils.module';
 import { MidtransModule } from 'src/midtrans/midtrans.module';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
-  imports: [UtilsModule, MidtransModule],
+  imports: [UtilsModule, MidtransModule, NotificationsModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService],
