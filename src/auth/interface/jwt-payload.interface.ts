fc970bcd36962e738f6a834736795e9978fcd455
Farhan Aziz <farhan@aththariq.com>
2024-02-12 16:39:44 +0700

    Update JwtPayload interface to include user role using Prisma's UserRole

--- src/auth/interface/jwt-payload.interface.ts
+++ src/auth/interface/jwt-payload.interface.ts
@@ -1,3 +1,6 @@
-export interface JwtPayload {
-  user: { id: string; role: string };
+import { UserRole } from '@prisma/client';
+import { JwtPayload as DefaultJwtPayload } from 'jsonwebtoken';
+
+export interface JwtPayload extends DefaultJwtPayload {
+  user: { id: string; role: UserRole };
 }
