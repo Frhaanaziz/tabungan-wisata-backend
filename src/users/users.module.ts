8db410a662c6354b8689f355d36a083dc4b40368
Farhan Aziz <farhan@aththariq.com>
2024-02-12 20:57:08 +0700

    Add NotificationsModule to imports in users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,15 @@ import { UsersController } from './users.controller';
 import { UtilsModule } from 'src/utils/utils.module';
 import { VerificationsModule } from 'src/verifications/verifications.module';
 import { PaymentsModule } from 'src/payments/payments.module';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
-  imports: [UtilsModule, VerificationsModule, PaymentsModule],
+  imports: [
+    UtilsModule,
+    VerificationsModule,
+    PaymentsModule,
+    NotificationsModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
