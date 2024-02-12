83e53b832758034ae8da336a04c124e545157180
Farhan Aziz <farhan@aththariq.com>
2024-02-12 16:40:10 +0700

    Add notifications module

---
+++ src/notifications/notifications.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { NotificationsService } from './notifications.service';
+import { NotificationsController } from './notifications.controller';
+import { NotificationsGateway } from './notifications.gateway';
+import { UtilsModule } from 'src/utils/utils.module';
+
+@Module({
+  imports: [UtilsModule],
+  controllers: [NotificationsController],
+  providers: [NotificationsService, NotificationsGateway],
+  exports: [NotificationsService, NotificationsGateway],
+})
+export class NotificationsModule {}
