08c7f206d386468687949e2d62adeb1405dd5c46
Farhan Aziz <farhan@aththariq.com>
2024-02-12 17:40:34 +0700

    Add PrismaService to NotificationsController constructor

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { CreateNotificationDto } from './dto/create-notification.dto';
+import { PrismaService } from 'nestjs-prisma';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private readonly notificationsService: NotificationsService) {}
+  constructor(
+    private readonly notificationsService: NotificationsService,
+    private readonly prisma: PrismaService,
+  ) {}
   @Get(':id')
   async getNotification(@Param('id') id: string) {
     return this.notificationsService.getNotificationOrFail({ id });
@@ -21,4 +25,32 @@ export class NotificationsController {
       },
     });
   }
+
+  @Post(':id/mark-all')
+  async markAllNotification(@Param('id') id: string) {
+    return this.prisma.notification.updateMany({
+      where: {
+        userId: id,
+        isRead: false,
+      },
+      data: {
+        isRead: true,
+      },
+    });
+  }
+
+  // @Patch(':id/isRead')
+  // async markNotification(
+  //   @Param('id') id: string,
+  //   @Body('isRead') { isRead }: UpdateNotificationDto,
+  // ) {
+  //   return this.notificationsService.updateNotification({
+  //     where: {
+  //       id,
+  //     },
+  //     data: {
+  //       isRead,
+  //     },
+  //   });
+  // }
 }
