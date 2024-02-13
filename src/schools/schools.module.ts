eda599f2917d9c5617caeabe990e96312db8f276
Farhan Aziz <farhan@aththariq.com>
2024-02-13 20:56:24 +0700

    Add EventRegistrationsModule to imports in schools.module.ts

--- src/schools/schools.module.ts
+++ src/schools/schools.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SchoolsService } from './schools.service';
 import { SchoolsController } from './schools.controller';
 import { UtilsModule } from 'src/utils/utils.module';
+import { EventRegistrationsModule } from 'src/event-registrations/event-registrations.module';
 
 @Module({
-  imports: [UtilsModule],
+  imports: [UtilsModule, EventRegistrationsModule],
   controllers: [SchoolsController],
   providers: [SchoolsService],
   exports: [SchoolsService],
