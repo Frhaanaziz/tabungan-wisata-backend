8e9658b887cd6dc910d74d5567d0db8e9c7a7829
Farhan Aziz <farhan@aththariq.com>
2024-02-17 23:26:37 +0700

    Add OnlineUsersGateway to UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,6 +5,7 @@ import { UtilsModule } from 'src/utils/utils.module';
 import { VerificationsModule } from 'src/verifications/verifications.module';
 import { PaymentsModule } from 'src/payments/payments.module';
 import { NotificationsModule } from 'src/notifications/notifications.module';
+import { OnlineUsersGateway } from './online.gateway';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { NotificationsModule } from 'src/notifications/notifications.module';
     NotificationsModule,
   ],
   controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService],
+  providers: [UsersService, OnlineUsersGateway],
+  exports: [UsersService, OnlineUsersGateway],
 })
 export class UsersModule {}
