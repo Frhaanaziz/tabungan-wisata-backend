cb0fab1085ac12a9b25eb3325929ae7e865947bf
Farhan Aziz <farhan@aththariq.com>
2024-02-17 23:24:12 +0700

    Remove import of UtilsModule in files.module.ts

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
-import { UtilsModule } from 'src/utils/utils.module';
-
 @Module({
-  imports: [UtilsModule],
   controllers: [FilesController],
   providers: [FilesService],
 })
